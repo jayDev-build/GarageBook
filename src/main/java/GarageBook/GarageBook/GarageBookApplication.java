8045b300713965140bcc94ba95ecdf8a87dd4177
Yashitjain <jainyashit11@gmail.com>
2026-05-29 18:32:53 +0530

    initialize

---
+++ src/main/java/GarageBook/GarageBook/GarageBookApplication.java
@@ -0,0 +1,13 @@
+package GarageBook.GarageBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarageBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarageBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GarageBook/GarageBook/GarageBookApplicationTests.java
@@ -0,0 +1,13 @@
+package GarageBook.GarageBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarageBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
