08262ab6643bd3ed2ae9d7564ca4dd71bcf22855
Yashitjain <jainyashit11@gmail.com>
2026-06-05 00:24:43 +0530

    GarageController + Thymeleaf template

--- src/main/java/GarageBook/GarageBook/Repository/GarageRepository.java
+++ src/main/java/GarageBook/GarageBook/Repository/GarageRepository.java
@@ -1,6 +1,5 @@
 package GarageBook.GarageBook.Repository;
 
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
